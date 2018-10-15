f471209c5f8538451e9d66e1fd19a1e9237b9ac8
Joey Tepperman <jmichaelt98@gmail.com>
2018-10-15 16:06:03 -0400

    Update component to better match mocks
    Refactored the background to be its own separate component to allow it to have aline on top (as in the mocks), also fixed some spacing issues

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -4,73 +4,96 @@ import podcast from "../assets/img/podcast.png";
 import ActionButton from "./ActionButton";
 
 const Podcast = () => (
-  <div
-    css={{
-      paddingBottom: "133px",
-      background: `url(${circuits}) no-repeat center 180px`,
-      backgroundSize: "100%",
-      marginBottom: "-300px",
-      paddingLeft: "145px",
-      paddingRight: "145px",
-      "@media (max-width: 1128px)": {
-        background: `url(${circuits}) no-repeat center 420px`,
-        backgroundSize: "auto 315px"
-      },
-      "@media (max-width: 780px)": {
-        padding: "0px 16px 160px 16px"
-      }
-    }}
-  >
+  <div>
     <div
       css={{
-        padding: "60px 72px 60px 72px",
-        backgroundColor: "#ffffff",
-        borderRadius: "8px",
-        boxShadow: "4px 9px 40px 2px rgba(7, 16, 75, 0.1)",
-        border: "solid 1px #e8e8e8",
-        display: "grid",
-        gridTemplateColumns: "min-content auto",
-        "@media (max-width: 1128px)": { display: "block" },
-        padding: "40px 28px 40px 28px",
-        width: "100%"
+        paddingBottom: "133px",
+        width: "100%",
+        paddingLeft: "145px",
+        paddingRight: "145px",
+        "@media (max-width: 780px)": {
+          padding: "0px 16px 160px 16px"
+        },
+        position: "relative"
       }}
     >
-      <div css={{ "@media (max-width: 1128px)": { textAlign: "center" } }}>
-        <img
-          src={podcast}
-          css={{
-            "@media (max-width: 1128px)": {
-              width: "100%"
-            },
-            maxWidth: "290px"
-          }}
-        />
-      </div>
       <div
         css={{
-          textAlign: "left",
-          paddingLeft: "64px",
-          "@media (max-width: 1128px)": {
-            paddingLeft: "0px",
-            paddingTop: "46px"
-          }
+          borderTop: "solid red 10px",
+          position: "absolute",
+          bottom: "314px"
+        }}
+      />
+      <div
+        css={{
+          position: "relative",
+          padding: "60px 72px 60px 72px",
+          backgroundColor: "#ffffff",
+          borderRadius: "8px",
+          boxShadow: "4px 9px 40px 2px rgba(7, 16, 75, 0.1)",
+          border: "solid 1px #e8e8e8",
+          display: "grid",
+          gridTemplateColumns: "min-content auto",
+          "@media (max-width: 1128px)": { display: "block" },
+          padding: "40px 28px 40px 28px",
+          width: "100%"
         }}
       >
-        <h1 css={{ paddingBottom: "24px" }}>Listen to our podcast!</h1>
-        <p css={{ lineHeight: "1.5", paddingBottom: "40px" }}>
-          Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras
-          ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta.
-          Nam condimentum vitae ligula vel ornare.
-        </p>
-        <ActionButton
-          type="rounded"
-          foregroundColor="#00205b"
-          backgroundColor="#ffffff"
+        <div
+          css={{
+            paddingLeft: "72px",
+            "@media (max-width: 1128px)": { textAlign: "center" }
+          }}
         >
-          Listen
-        </ActionButton>
+          <img
+            src={podcast}
+            css={{
+              "@media (max-width: 1128px)": {
+                width: "100%"
+              },
+              maxWidth: "290px"
+            }}
+          />
+        </div>
+        <div
+          css={{
+            textAlign: "left",
+            paddingLeft: "64px",
+            "@media (max-width: 1128px)": {
+              paddingLeft: "0px",
+              paddingTop: "46px"
+            }
+          }}
+        >
+          <h1 css={{ paddingBottom: "24px", paddingTop: "10px" }}>
+            Listen to our podcast!
+          </h1>
+          <p css={{ lineHeight: "1.5", paddingBottom: "40px" }}>
+            Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras
+            ullamcorper bibendum bibendum. Duis tincidunt urna non pretium
+            porta. Nam condimentum vitae ligula vel ornare.
+          </p>
+          <ActionButton
+            type="rounded"
+            foregroundColor="#00205b"
+            backgroundColor="#ffffff"
+          >
+            Listen
+          </ActionButton>
+        </div>
       </div>
     </div>
+    <div
+      css={{
+        marginTop: "-315px",
+        height: "315px",
+        zIndex: "-1",
+        position: "relative",
+        background: `url(${circuits}) no-repeat center center`,
+        backgroundSize: "cover",
+        borderTop: "solid #dbdbdb 3px"
+      }}
+    />
   </div>
 );
 
